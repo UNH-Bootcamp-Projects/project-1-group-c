66a434d1c4109e09254f5060e08ba2881bd8e2d7
Corey Nance <coreynance83@gmail.com>
2021-04-20 11:04:37 -0400

    modified js file to create restaurant list and added a little style using css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 let apiKey = "5c51a06114504d42a38134910bd6bd9a";
 let googleApi = "AIzaSyClKhFXOi6w6MwKpiSsIlmAEtkTnd9132s";
 let userZip;
-
+let theList = document.querySelector("#testTwo");
 let restaurant;
+let restaurantArray = []; // currently not being used
+let newArray = []; // currently not being used
+
 
 
 function getUserLocation () {
@@ -17,15 +20,15 @@ function getUserLocation () {
                 console.log(data)
                 userZip = data.postal_code
                 console.log(userZip)
-                displayRestaurantsNearMe()
+                getRestaurantsNearMe()
                 
                 
             })
         }
 
-function displayRestaurantsNearMe () {
+function getRestaurantsNearMe () {
     let proxy = "https://ben.hutchins.co/proxy/fetch/"
-    let restaurantUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restuarants+in+${userZip}&key=${googleApi}`;
+    let restaurantUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restuarants+in+${userZip}&key=${googleApi}&`;
 
    
 
@@ -36,22 +39,74 @@ function displayRestaurantsNearMe () {
         })
         .then (function (data) {
             console.log(data)
-            for (let i = 0; i < 20; i++) {
-                restaurant = {
-                restName: data.results[i].name,
-                address: data.results[i].formatted_address,
-                priceLevel: data.results[i].price_level,
-                rating: data.results[i].rating,
-                openNow: data.results[i].opening_hours.open_now,
-                }
-                
-                 
-                
             
-        }
-        console.log(restaurant) 
-          })
-        }
+            for (let i = 0; i < data.results.length; i++) {
+
+                /* with using the forloop that was already created.   I added the if statement so to 
+                account for businesses that were closed.  Reason being is that the openNow variable would
+                crash when the business is not operational.  opening_hours.open_now doesnt exist on the close 
+                businesses.*/
+                if(data.results[i].business_status === "OPERATIONAL") {
+                
+                    // this block here creates the li and  attaches to the empty <ul> in the html.
+                    let listElement = document.createElement('li');
+                    listElement.classList.add("listElementClass");
+                    listElement.id = "list" + i;
+                    theList.appendChild(listElement);  
+    
+                //   this block here is what creates a dynamic ul and attaches it to the dynamic li
+                    let newList = document.querySelector("#list" + i);
+                    let restaurantList = document.createElement('ul');
+                    restaurantList.classList.add("restListClass");
+                    restaurantList.id = "restList" + i;
+                    newList.append(restaurantList);
+
+
+                    /* And this  block here is finally where all the list we see is created.
+                    This part creates the li elements then attactes the text content pulled from the api 
+                    then attacthes all that to the dynamic li element. */
+                    let theRestaurant = document.querySelector("#restList" + i);
+                    let restName = data.results[i].name;
+                    let address = data.results[i].formatted_address;
+                    let priceLevel = data.results[i].price_level;
+                    let rating = data.results[i].rating;
+                    let openNow = data.results[i].opening_hours.open_now;
+
+                    // here is the long and lat elements but havent figured out how to save to array yet
+                    let restLat = data.results[i].geometry.location.lat;
+                    let restLng = data.results[i].geometry.location.lng; 
+
+                    let listName = document.createElement("h5");
+                    let listAddress = document.createElement('li')
+                    let listPriceLevel = document.createElement("li");
+                    let listRating = document.createElement("li");
+                    let listOpenNow = document.createElement("li");
+
+                    listName.textContent = "Restaurant Name: " + restName;
+                    listAddress.textContent = "Restaurant Address: " + address;
+                    listPriceLevel.textContent = "Price Level: " + priceLevel;
+                    listRating.textContent = "Restaurant Rating: " + rating;
+                    listOpenNow.textContent = "Is Open Now: " + openNow;
+
+                    theRestaurant.appendChild(listName);
+                    theRestaurant.appendChild(listAddress);
+                    theRestaurant.appendChild(listPriceLevel);
+                    theRestaurant.appendChild(listRating);
+                    theRestaurant.appendChild(listOpenNow);
+                    
+
             
 
+                    
+               }
+         }
+        
+           
+          })
+          
+        }
+      
+
 getUserLocation();
+
+
